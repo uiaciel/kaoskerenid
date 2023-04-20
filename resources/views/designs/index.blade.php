1175a6855f5adb3dec86ec646ea793ba0e9a7b25
uiaciel <uiaciel@gmail.com>
2023-04-20 11:41:34 +0700

    Design Random

--- resources/views/designs/index.blade.php
+++ resources/views/designs/index.blade.php
@@ -1,7 +1,54 @@
-@extends('layouts.app')
-@section('title', 'Page Title')
-@section('content')
-<div class="row">
-
-</div>
-@endsection
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <h2 class="text-white">Random Mockup</h2>
+
+
+        @forelse ($designs as $image)
+            <div class="col-md-3">
+                <div class="card mb-3">
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#mockup-{{ $image->id }}"><img
+                            src="{{ $image->path }}" class="card-img-top" alt="..." loading="lazy"></a>
+
+                    <div class="card-body">
+                        {{-- <h5 class="card-title">Card title</h5> --}}
+                        {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p> --}}
+
+                    </div>
+                </div>
+                <div class="modal fade" id="mockup-{{ $image->id }}" tabindex="-1" data-bs-backdrop="static"
+                    data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">MOCKUP</h5>
+
+
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="{{ $image->path }}" class="img-fluid" loading="lazy" />
+
+                            </div>
+                            <div class="modal-footer">
+                                <form action="{{ route('design.destroy', $image->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <h3>Data Kosong</h3>
+        @endforelse
+
+    </div>
+@endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -123,7 +123,7 @@
                 <div class="row">
 
                     <div class="col-lg-6 col-12">
-                        <h2 class="text-white mb-lg-0">KAOSKEREN.ID</h2>
+                        <h2 class="text-white mb-lg-0">SABLON SATUAN</h2>
                     </div>
 
                     <div class="col-lg-6 col-12 d-flex justify-content-lg-end align-items-center">
